2b9c8c0da1d644c8661daf3274d0db936e16f747
Albarra Erdanto <albarraerdanto@gmail.com>
2020-08-11 12:15:42 +0700

    Seperti sebelumnya, namun ini file yang baru

---
+++ app/src/main/java/com/example/githubuserapi/adapter/SectionsPagerAdapter.kt
@@ -0,0 +1,31 @@
+package com.example.githubuserapi.adapter
+
+import android.content.Context
+import androidx.annotation.Nullable
+import androidx.annotation.StringRes
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.example.githubuserapi.R
+import com.example.githubuserapi.fragment.FollowerFragment
+import com.example.githubuserapi.fragment.FollowingFragment
+
+class SectionsPagerAdapter(private val mContext: Context, fm: FragmentManager): FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+    @StringRes
+    private val TAB_TITLES = intArrayOf(R.string.tab_followers, R.string.tab_following)
+    override fun getItem(position: Int): Fragment {
+        var fragment: Fragment? = null
+        when (position) {
+            0 -> fragment = FollowerFragment()
+            1 -> fragment = FollowingFragment()
+        }
+        return fragment as Fragment
+    }
+
+    @Nullable
+    override fun getPageTitle(position: Int): CharSequence? {
+        return mContext.resources.getString(TAB_TITLES[position])
+    }
+
+    override fun getCount(): Int = 2
+}
